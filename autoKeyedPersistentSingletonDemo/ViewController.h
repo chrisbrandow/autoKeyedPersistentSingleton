9f57c9af3ca896d48d79742d874550be3e0a900f
chrisbrandow <chrisbrandow@gmail.com>
2014-10-03 12:38:37 -0700

    Initial Commit

---
+++ autoKeyedPersistentSingletonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autoKeyedPersistentSingletonDemo
+//
+//  Created by Christopher Brandow on 10/3/14.
+//  Copyright (c) 2014 Flouu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
